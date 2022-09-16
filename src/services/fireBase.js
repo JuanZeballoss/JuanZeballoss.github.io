bb6c1a2c4a6330dc7e5f35a5e6a6948b7ff9b669
JuanZeballoss <juanzeballoss@outlook.com>
2022-09-16 08:27:18 -0300

    Eliminando collection de firebase

--- src/services/fireBase.js
+++ src/services/fireBase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-import { addDoc, collection, getFirestore } from "firebase/firestore"
+import { addDoc, getFirestore } from "firebase/firestore"
 
 import stockProducts from "../components/Data/Data"
 
